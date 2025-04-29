f66d83f398df85473ae88f54da6e59ac95bb260d
shelssss <shelsssspal@gmail.com>
2025-04-29 15:27:08 +0800

    Added hashing for password

---
+++ BogsyVideoStore/Helpers/PassHash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace BogsyVideoStore.Helpers
+{
+    public static class PassHash
+    {
+        public static string HashPassword(string plainPassword)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(plainPassword);
+        }
+
+       
+        public static bool VerifyPassword(string plainPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+        }
+    }
+}
