e19a43ee5dc8ac5d318632126f9094cdfa24b9b1
shelssss <shelsssspal@gmail.com>
2025-05-01 12:29:19 +0800

    Add a class for the close child MDI function

---
+++ BogsyVideoStore/Helpers/CloseChildrenMDI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogsyVideoStore.Helpers
+{
+    public class CloseChildrenMDI: Form
+    {
+        public void CloseAllChildForms()
+        {
+            foreach (Form child in this.MdiChildren)
+            {
+                child.Close();
+            }
+        }
+    }
+}
