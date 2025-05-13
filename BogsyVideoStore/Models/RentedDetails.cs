59c2bca1d5ccd3cbcc1ae3f2c846825986642880
shelssss <shelsssspal@gmail.com>
2025-05-13 17:13:20 +0800

    Added Model handler for joins table

---
+++ BogsyVideoStore/Models/RentedDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogsyVideoStore.Models
+{
+    public class RentedDetails
+    {
+        public string CustomerUsername { get; set; }
+        public string VideoRented { get; set; }
+        public string Category { get; set; }
+        public int MaxRentDays { get; set; }
+        public DateOnly RentedDate { get; set; }
+        public DateOnly? ReturnedDate { get; set; }
+        public int RentCost { get; set; }
+        public int LateReturnFee { get; set; }
+        public string Status { get; set; }
+    }
+}
