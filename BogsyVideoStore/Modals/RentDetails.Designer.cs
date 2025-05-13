231dbc9822118a8fe9d284988f774f2d499d75e0
shelssss <shelsssspal@gmail.com>
2025-05-13 17:12:51 +0800

    Added new form for the print receipt

---
+++ BogsyVideoStore/Modals/RentDetails.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace BogsyVideoStore.Modals
+{
+    partial class RentDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            custInfoLlb = new Label();
+            label1 = new Label();
+            dateTodayLbl = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            custNameLbl = new Label();
+            titleLbl = new Label();
+            catLbl = new Label();
+            dateRentedLbl = new Label();
+            maxRentLbl = new Label();
+            costLbl = new Label();
+            ovedueLbl = new Label();
+            totalLbl = new Label();
+            label10 = new Label();
+            returnedLbl = new Label();
+            printReceipt = new System.Drawing.Printing.PrintDocument();
+            printBtn = new Button();
+            printDialog = new PrintDialog();
+            SuspendLayout();
+            // 
+            // custInfoLlb
+            // 
+            custInfoLlb.AutoSize = true;
+            custInfoLlb.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            custInfoLlb.ForeColor = Color.FromArgb(0, 46, 68);
+            custInfoLlb.Location = new Point(104, 28);
+            custInfoLlb.Name = "custInfoLlb";
+            custInfoLlb.Size = new Size(192, 30);
+            custInfoLlb.TabIndex = 13;
+            custInfoLlb.Text = "Bogsy Video Store";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(0, 46, 68);
+            label1.Location = new Point(136, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 15);
+            label1.TabIndex = 14;
+            label1.Text = "Customer Rent Details";
+            // 
+            // dateTodayLbl
+            // 
+            dateTodayLbl.AutoSize = true;
+            dateTodayLbl.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTodayLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            dateTodayLbl.Location = new Point(136, 78);
+            dateTodayLbl.Name = "dateTodayLbl";
+            dateTodayLbl.Size = new Size(0, 15);
+            dateTodayLbl.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(0, 46, 68);
+            label2.Location = new Point(27, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 17);
+            label2.TabIndex = 16;
+            label2.Text = "Name ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(0, 46, 68);
+            label3.Location = new Point(27, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(352, 15);
+            label3.TabIndex = 17;
+            label3.Text = "_____________________________________________________________________";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(0, 46, 68);
+            label4.Location = new Point(27, 280);
+            label4.Name = "label4";
+            label4.Size = new Size(352, 15);
+            label4.TabIndex = 19;
+            label4.Text = "_____________________________________________________________________";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label5.ForeColor = Color.FromArgb(0, 46, 68);
+            label5.Location = new Point(27, 162);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 17);
+            label5.TabIndex = 18;
+            label5.Text = "Rented Video";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label6.ForeColor = Color.FromArgb(0, 46, 68);
+            label6.Location = new Point(27, 210);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 17);
+            label6.TabIndex = 20;
+            label6.Text = "Date Rented";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label7.ForeColor = Color.FromArgb(0, 46, 68);
+            label7.Location = new Point(27, 234);
+            label7.Name = "label7";
+            label7.Size = new Size(106, 17);
+            label7.TabIndex = 21;
+            label7.Text = "Max Rent Day/s";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label8.ForeColor = Color.FromArgb(0, 46, 68);
+            label8.Location = new Point(27, 186);
+            label8.Name = "label8";
+            label8.Size = new Size(104, 17);
+            label8.TabIndex = 22;
+            label8.Text = "Video Category";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label9.ForeColor = Color.FromArgb(0, 46, 68);
+            label9.Location = new Point(27, 329);
+            label9.Name = "label9";
+            label9.Size = new Size(91, 17);
+            label9.TabIndex = 27;
+            label9.Text = "Overdue Fees";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label11.ForeColor = Color.FromArgb(0, 46, 68);
+            label11.Location = new Point(27, 372);
+            label11.Name = "label11";
+            label11.Size = new Size(39, 17);
+            label11.TabIndex = 25;
+            label11.Text = "Total";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.FromArgb(0, 46, 68);
+            label12.Location = new Point(27, 346);
+            label12.Name = "label12";
+            label12.Size = new Size(352, 15);
+            label12.TabIndex = 24;
+            label12.Text = "_____________________________________________________________________";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label13.ForeColor = Color.FromArgb(0, 46, 68);
+            label13.Location = new Point(27, 305);
+            label13.Name = "label13";
+            label13.Size = new Size(75, 17);
+            label13.TabIndex = 23;
+            label13.Text = "Video Cost";
+            // 
+            // custNameLbl
+            // 
+            custNameLbl.AutoSize = true;
+            custNameLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            custNameLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            custNameLbl.Location = new Point(297, 114);
+            custNameLbl.Name = "custNameLbl";
+            custNameLbl.Size = new Size(47, 17);
+            custNameLbl.TabIndex = 28;
+            custNameLbl.Text = "Name ";
+            // 
+            // titleLbl
+            // 
+            titleLbl.AutoSize = true;
+            titleLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            titleLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            titleLbl.Location = new Point(297, 162);
+            titleLbl.Name = "titleLbl";
+            titleLbl.Size = new Size(87, 17);
+            titleLbl.TabIndex = 29;
+            titleLbl.Text = "Rented Video";
+            // 
+            // catLbl
+            // 
+            catLbl.AutoSize = true;
+            catLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            catLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            catLbl.Location = new Point(297, 186);
+            catLbl.Name = "catLbl";
+            catLbl.Size = new Size(99, 17);
+            catLbl.TabIndex = 30;
+            catLbl.Text = "Video Category";
+            // 
+            // dateRentedLbl
+            // 
+            dateRentedLbl.AutoSize = true;
+            dateRentedLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateRentedLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            dateRentedLbl.Location = new Point(297, 210);
+            dateRentedLbl.Name = "dateRentedLbl";
+            dateRentedLbl.Size = new Size(80, 17);
+            dateRentedLbl.TabIndex = 31;
+            dateRentedLbl.Text = "Date Rented";
+            // 
+            // maxRentLbl
+            // 
+            maxRentLbl.AutoSize = true;
+            maxRentLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            maxRentLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            maxRentLbl.Location = new Point(297, 234);
+            maxRentLbl.Name = "maxRentLbl";
+            maxRentLbl.Size = new Size(100, 17);
+            maxRentLbl.TabIndex = 32;
+            maxRentLbl.Text = "Max Rent Day/s";
+            // 
+            // costLbl
+            // 
+            costLbl.AutoSize = true;
+            costLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            costLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            costLbl.Location = new Point(297, 305);
+            costLbl.Name = "costLbl";
+            costLbl.Size = new Size(72, 17);
+            costLbl.TabIndex = 33;
+            costLbl.Text = "Video Cost";
+            // 
+            // ovedueLbl
+            // 
+            ovedueLbl.AutoSize = true;
+            ovedueLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ovedueLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            ovedueLbl.Location = new Point(297, 329);
+            ovedueLbl.Name = "ovedueLbl";
+            ovedueLbl.Size = new Size(88, 17);
+            ovedueLbl.TabIndex = 34;
+            ovedueLbl.Text = "Overdue Fees";
+            // 
+            // totalLbl
+            // 
+            totalLbl.AutoSize = true;
+            totalLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            totalLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            totalLbl.Location = new Point(297, 372);
+            totalLbl.Name = "totalLbl";
+            totalLbl.Size = new Size(36, 17);
+            totalLbl.TabIndex = 35;
+            totalLbl.Text = "Total";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label10.ForeColor = Color.FromArgb(0, 46, 68);
+            label10.Location = new Point(27, 262);
+            label10.Name = "label10";
+            label10.Size = new Size(97, 17);
+            label10.TabIndex = 36;
+            label10.Text = "Date Returned";
+            // 
+            // returnedLbl
+            // 
+            returnedLbl.AutoSize = true;
+            returnedLbl.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            returnedLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            returnedLbl.Location = new Point(296, 263);
+            returnedLbl.Name = "returnedLbl";
+            returnedLbl.Size = new Size(100, 17);
+            returnedLbl.TabIndex = 37;
+            returnedLbl.Text = "Max Rent Day/s";
+            // 
+            // printBtn
+            // 
+            printBtn.BackColor = Color.FromArgb(0, 46, 68);
+            printBtn.FlatStyle = FlatStyle.Popup;
+            printBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            printBtn.ForeColor = Color.White;
+            printBtn.Location = new Point(161, 412);
+            printBtn.Name = "printBtn";
+            printBtn.Size = new Size(75, 23);
+            printBtn.TabIndex = 38;
+            printBtn.Text = "Print";
+            printBtn.UseVisualStyleBackColor = false;
+            printBtn.Click += printBtn_Click;
+            // 
+            // printDialog
+            // 
+            printDialog.UseEXDialog = true;
+            // 
+            // RentDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(413, 447);
+            Controls.Add(printBtn);
+            Controls.Add(returnedLbl);
+            Controls.Add(label10);
+            Controls.Add(totalLbl);
+            Controls.Add(ovedueLbl);
+            Controls.Add(costLbl);
+            Controls.Add(maxRentLbl);
+            Controls.Add(dateRentedLbl);
+            Controls.Add(catLbl);
+            Controls.Add(titleLbl);
+            Controls.Add(custNameLbl);
+            Controls.Add(label9);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dateTodayLbl);
+            Controls.Add(label1);
+            Controls.Add(custInfoLlb);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "RentDetails";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "RentDetails";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label custInfoLlb;
+        private Label label1;
+        public Label dateTodayLbl;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        public Label custNameLbl;
+        public Label titleLbl;
+        public Label catLbl;
+        public Label dateRentedLbl;
+        public Label maxRentLbl;
+        public Label costLbl;
+        public Label ovedueLbl;
+        public Label totalLbl;
+        private Label label10;
+        public Label returnedLbl;
+        private System.Drawing.Printing.PrintDocument printReceipt;
+        private Button printBtn;
+        private PrintDialog printDialog;
+    }
+}
---
+++ BogsyVideoStore/Modals/RentDetails.cs
@@ -0,0 +1,49 @@
+﻿using BogsyVideoStore.Helpers;
+using BogsyVideoStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BogsyVideoStore.Modals
+{
+    public partial class RentDetails : Form
+    {
+        private CustomerRented _customer;
+        private PrintDocument printDocument;
+        public RentDetails()
+        {
+            InitializeComponent();
+        }
+
+        public RentDetails(RentedDetails customer)
+        {
+            InitializeComponent();
+
+            dateTodayLbl.Text = DateTime.Now.ToString();
+            custNameLbl.Text = customer.CustomerUsername;
+            titleLbl.Text = customer.VideoRented;
+            catLbl.Text = customer.Category;
+            dateRentedLbl.Text = customer.RentedDate.ToString();
+            maxRentLbl.Text = customer.MaxRentDays.ToString();
+            returnedLbl.Text = customer.ReturnedDate?.ToString() ?? "Not Yet Returned";
+            costLbl.Text = customer.RentCost.ToString("C");
+            ovedueLbl.Text = customer.LateReturnFee.ToString("C");
+            totalLbl.Text = (customer.RentCost + customer.LateReturnFee).ToString("C");
+
+        }
+
+        private void printBtn_Click(object sender, EventArgs e)
+        {
+            PrintModule.PrintRentalDetails(this);
+        }
+
+        
+    }
+}
