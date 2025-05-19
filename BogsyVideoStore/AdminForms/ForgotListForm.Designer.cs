20612bf0e258625ca66f5ce56a3b8ed4c1630caa
shelssss <shelsssspal@gmail.com>
2025-05-19 14:23:09 +0800

    Added Change Password Form

---
+++ BogsyVideoStore/AdminForms/ForgotListForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BogsyVideoStore.AdminForms
+{
+    partial class ForgotListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            nameLbl = new Label();
+            searchBar = new TextBox();
+            AddCustomerBtn = new Button();
+            ForgotListGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)ForgotListGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // nameLbl
+            // 
+            nameLbl.AutoSize = true;
+            nameLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            nameLbl.ForeColor = Color.FromArgb(0, 46, 68);
+            nameLbl.Location = new Point(80, 10);
+            nameLbl.Name = "nameLbl";
+            nameLbl.Size = new Size(46, 15);
+            nameLbl.TabIndex = 7;
+            nameLbl.Text = "Name: ";
+            // 
+            // searchBar
+            // 
+            searchBar.Location = new Point(80, 28);
+            searchBar.Name = "searchBar";
+            searchBar.PlaceholderText = "Search...";
+            searchBar.Size = new Size(165, 23);
+            searchBar.TabIndex = 8;
+            searchBar.TextChanged += searchBar_TextChanged;
+            // 
+            // AddCustomerBtn
+            // 
+            AddCustomerBtn.BackColor = Color.FromArgb(0, 46, 68);
+            AddCustomerBtn.FlatStyle = FlatStyle.Popup;
+            AddCustomerBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AddCustomerBtn.ForeColor = SystemColors.ButtonHighlight;
+            AddCustomerBtn.Location = new Point(698, 28);
+            AddCustomerBtn.Name = "AddCustomerBtn";
+            AddCustomerBtn.Size = new Size(75, 23);
+            AddCustomerBtn.TabIndex = 9;
+            AddCustomerBtn.Text = "Add";
+            AddCustomerBtn.UseVisualStyleBackColor = false;
+            // 
+            // ForgotListGrid
+            // 
+            ForgotListGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            ForgotListGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ForgotListGrid.Location = new Point(80, 57);
+            ForgotListGrid.Name = "ForgotListGrid";
+            ForgotListGrid.ReadOnly = true;
+            ForgotListGrid.Size = new Size(693, 433);
+            ForgotListGrid.TabIndex = 10;
+            ForgotListGrid.CellContentClick += ForgotListGrid_CellContentClick;
+            // 
+            // ForgotListForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(802, 539);
+            Controls.Add(ForgotListGrid);
+            Controls.Add(AddCustomerBtn);
+            Controls.Add(searchBar);
+            Controls.Add(nameLbl);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ForgotListForm";
+            Text = "ForgotListForm";
+            ((System.ComponentModel.ISupportInitialize)ForgotListGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label nameLbl;
+        private TextBox searchBar;
+        private Button AddCustomerBtn;
+        private DataGridView ForgotListGrid;
+    }
+}
---
+++ BogsyVideoStore/AdminForms/ForgotListForm.cs
@@ -0,0 +1,83 @@
+﻿using BogsyVideoStore.Helpers;
+using BogsyVideoStore.Modals;
+using BogsyVideoStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BogsyVideoStore.AdminForms
+{
+    public partial class ForgotListForm : Form
+    {
+        public ForgotListForm()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void ForgotListForm_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (var context = new AppDbContext())
+            {
+                var queries = context.CustomerQueries.ToList();
+                ForgotListGrid.DataSource = queries;
+                DisplayHelper.DisplayDefaults(ForgotListGrid);
+            }
+        }
+
+        private void searchBar_TextChanged(object sender, EventArgs e)
+        {
+            using (var context = new AppDbContext())
+            {
+                string searchValue = searchBar.Text.Trim().ToLower();
+
+                var filtered = FilterResults.SearchCustomerQuery(context, searchValue);
+
+                ForgotListGrid.DataSource = filtered;
+                DisplayHelper.DisplayDefaults(ForgotListGrid);
+            }
+        }
+
+        private void ForgotListGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                using (var context = new AppDbContext())
+                {   
+                    var selectedRow = ForgotListGrid.Rows[e.RowIndex];
+                    var query = (CustomerQueries)selectedRow.DataBoundItem;
+
+                    var username = query.Username;
+
+                    var customer = context.Customer.FirstOrDefault(c => c.Username == username);
+
+                    if(customer != null)
+                    {
+                        var changePassForm = new PasswordChange(customer);
+                        changePassForm.ShowDialog();
+                        LoadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show(StringHelpers.usernameNotFound);
+                    }
+                    
+
+                }
+               
+            }
+        }
+    }
+}
